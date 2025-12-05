8e77c9feac5f0640c679e58db0ce94350233717d
jgtux <joaovitorgdd@gmail.com>
2025-12-05 16:19:04 -0300

    logic fix

--- api/ai-ms/lang_setup.py
+++
@@ -1,9 +0,0 @@
-
-#Bilbliotecas que serão usadas durante o prjeto
-#Comando terminal:
-#windows
-#py -m pip install langchain langgraph langchain_groq python-dotenv langchain_community
-
-#Verificar versões
-#
-#
